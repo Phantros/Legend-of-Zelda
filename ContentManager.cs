475631d766e327c9d4764a77f06a16ef4f8f200f
Nick Wijker <nick_wijker@hotmail.com>
2020-05-04 22:32:38 +0200

    Simpel skelet van classes

---
+++ ContentManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legend_of_Zelda
+{
+    class ContentManager
+
+    {
+        private void LoadAssets()
+        {
+
+        }
+
+        
+    }
+}
